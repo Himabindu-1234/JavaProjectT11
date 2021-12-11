1eaefe96d831b02c34f894542f8dc34412a0e046
Chennanandini-1234 <95916725+Chennanandini-1234@users.noreply.github.com>
2021-12-11 07:28:28 +0530

    Update code1.java

--- src/code1.java
+++ src/code1.java
@@ -3,6 +3,7 @@ public class code1 {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		system.out.println("Hello am Pravin");
+		System.out.println("Hello World 1");
 		
 	}
 
